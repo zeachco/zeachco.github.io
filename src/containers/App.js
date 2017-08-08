872758a0186c371b400458216ff479dc15754a5b
Olivier Rousseau-Guyot <zeachco@gmail.com>
2017-08-07 21:18:50 -0400

    futuristic stack test

---
+++ src/containers/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <section className="hero">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Oh Hi!</h1>
+          <h2 className="subtitle">This is my github.</h2>
+          <p>
+            This project autobuild with Travis<br />
+            <img src="//travis-ci.org/zeachco/zeachco.github.io.svg?branch=master" alt="Travis status badge" />
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import './services/ga';
+import App from './containers/App';
+
+render(<App />, document.getElementById('app'));
